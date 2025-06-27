6396dc0b65c36b5a412cb0513da3240b40a118fc
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-06-27 12:13:35 +0530

    add local host database to system

--- src/main/java/com/example/RegistrationLoginPage/config/SecurityConfig.java
+++ src/main/java/com/example/RegistrationLoginPage/config/SecurityConfig.java
@@ -42,10 +42,10 @@ public class SecurityConfig {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:5173") // frontend origin
+                            .allowedOrigins("http://localhost:5173","http://localhost:5174") // frontend origin
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
-                            .allowCredentials(true);  // THIS IS THE CRUCIAL PART
+                            .allowCredentials(true);
                 }
             };
         }
