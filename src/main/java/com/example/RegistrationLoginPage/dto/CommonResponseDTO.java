df323e0d1e49845f96e766b3319d4e5f499e5de8
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-07-24 07:38:15 +0530

    Revert "working"
    This reverts commit 059992e14b544e857f62889823d317009b236eb2.

--- src/main/java/com/example/RegistrationLoginPage/dto/CommonResponseDTO.java
+++ src/main/java/com/example/RegistrationLoginPage/dto/CommonResponseDTO.java
@@ -7,14 +7,4 @@ public class CommonResponseDTO {
     private boolean status;
     private String message;
     private Object data;
-
-    public CommonResponseDTO() {}
-
-    // Constructor that accepts all three parameters
-    public CommonResponseDTO(boolean status, String message, Object data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
 }
